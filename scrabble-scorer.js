1cc38a7d0be7a4c9549c388b32325dcc6e11a4bb
mcmxmcm <mcmxmcm@pm.me>
2021-03-07 07:47:32 +0000

    changed 'scorerFunction' to 'scoringFunction' to please the autograder.

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -79,17 +79,17 @@ const scoringAlgorithms = [
 {
   name: 'Simple Score',
   description: 'Each letter is worth 1 point.',
-  scorerFunction: simpleScore
+  scoringFunction: simpleScore
 }, 
 {
   name: 'Bonus Vowels',
   description: 'Vowels are 3 pts, consonants are 1 pt.',
-  scorerFunction: vowelBonusScore
+  scoringFunction: vowelBonusScore
 }, 
 {
   name: 'Scrabble',
   description: 'The traditional scoring algorithm.',
-  scorerFunction: scrabbleScore
+  scoringFunction: scrabbleScore
 }];
 
 function scorerPrompt() {
@@ -133,7 +133,7 @@ let newPointStructure = transform(oldPointStructure);
 function runProgram() {
    let userWord = initialPrompt();
    let chosenAlgorithm = scorerPrompt();
-   let userScore = chosenAlgorithm.scorerFunction(userWord);
+   let userScore = chosenAlgorithm.scoringFunction(userWord);
    console.log(
       `
       .........................................
